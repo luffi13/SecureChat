82b4c215c872445a7c01fae2ed2a4d18d7322b53
Luffi Aditya Sandy <luffi.as@gmail.com>
2016-04-25 23:00:20 +0700

    chat+encryptionRSA+publickey disimpan di server

---
+++ KIJ_Chat_Client/src/kij_chat_client/Read.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kij_chat_client;
+
+/*import java.net.Socket;*/
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author santen-suru
+ */
+public class Read implements Runnable {
+        
+        private Scanner in;//MAKE SOCKET INSTANCE VARIABLE
+        String input;
+        boolean keepGoing = true;
+        ArrayList<String> log;
+	
+	public Read(Scanner in, ArrayList<String> log)
+	{
+		this.in = in;
+                this.log = log;
+	}
+    
+        @Override
+	public void run()//INHERIT THE RUN METHOD FROM THE Runnable INTERFACE
+	{
+		try
+		{
+			while (keepGoing)//WHILE THE PROGRAM IS RUNNING
+			{						
+				if(this.in.hasNext()) {
+                                                                   //IF THE SERVER SENT US SOMETHING
+                                        input = this.in.nextLine();
+					//PRINT IT OUT
+                                        if (input.split(" ")[0].toLowerCase().equals("success")) {
+                                            if (input.split(" ")[1].toLowerCase().equals("logout")) {
+                                                System.out.println(input);
+                                                keepGoing = false;
+                                                 
+                                            } else if (input.split(" ")[1].toLowerCase().equals("login")) {
+                                                System.out.println(input);
+                                                log.clear();
+                                                log.add("true");
+                                                
+                                            }
+                                            else{
+                                                String message = input.split(": ")[1].split("~~~||~~~")[0];//get message
+                                                String Pubk= input.split("~~~||~~~")[1];//get public key
+                                                ///decrypt
+                                                
+                                                
+                                                
+                                                System.out.println(input);
+                                            }
+                                        }
+                                        
+                                }
+                                
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();//MOST LIKELY WONT BE AN ERROR, GOOD PRACTICE TO CATCH THOUGH
+		} 
+	}
+}
