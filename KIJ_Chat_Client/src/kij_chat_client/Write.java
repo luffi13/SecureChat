70432cfbb4a2ed147e49c0c6cc4e4558c9955098
hanifsudira <sudirahanif@gmail.com>
2016-04-26 00:16:04 +0700

    hash login using sha-256

--- KIJ_Chat_Client/src/kij_chat_client/Write.java
+++ KIJ_Chat_Client/src/kij_chat_client/Write.java
@@ -9,7 +9,8 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 /**
  *
  * @author santen-suru
@@ -20,14 +21,30 @@ public class Write implements Runnable {
         private PrintWriter out;
         boolean keepGoing = true;
         ArrayList<String> log;
-	
+        
 	public Write(Scanner chat, PrintWriter out, ArrayList<String> log)
 	{
 		this.chat = chat;
                 this.out = out;
                 this.log = log;
 	}
-	
+        
+        public String ShaHashlogin(String password) throws NoSuchAlgorithmException{
+            
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+
+            byte byteData[] = md.digest();
+
+            //convert the byte to hex format method 1
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+             sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+     
+            return sb.toString();
+        }
+        
 	@Override
 	public void run()//INHERIT THE RUN METHOD FROM THE Runnable INTERFACE
 	{
@@ -36,7 +53,16 @@ public class Write implements Runnable {
 			while (keepGoing)//WHILE THE PROGRAM IS RUNNING
 			{						
 				String input = chat.nextLine();	//SET NEW VARIABLE input TO THE VALUE OF WHAT THE CLIENT TYPED IN
-				out.println(input);//SEND IT TO THE SERVER
+				
+                                
+                                if(input.split(" ")[0].toLowerCase().equals("login")){
+                                    String pass = input.split(" ")[2];
+                                    String hashpass = ShaHashlogin(pass);
+                                    input = input.split(" ")[0]+" "+input.split(" ")[1]+" "+hashpass;
+                                    //System.out.println(coba);
+                                } 
+                            
+                                out.println(input);//SEND IT TO THE SERVER
 				out.flush();//FLUSH THE STREAM
                                 
                                 if (input.contains("logout")) {
